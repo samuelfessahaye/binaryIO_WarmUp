e8087d7a1401645b3cead080d8926601153c6158
samuelfessahaye <sfessahaye@ufl.edu>
2025-03-11 14:47:34 -0400

    lab warmup

--- main.cpp
+++ main.cpp
@@ -6,18 +6,29 @@ int main() {
     std::fstream file("tips.shp", std::ios_base::binary | std::ios_base::in);
 
     if (!file.is_open()) {
-        std::cout << "Error opening file" << std::endl;
+        std::cerr << "Error opening file" << std::endl;
         return 1;
     }
 
     unsigned int num_strings;
     // TODO: Read number of strings
-    // Hint: file.read((char *)&num_strings, sizeof(num_strings));
+    file.read(reinterpret_cast<char*>(&num_strings), sizeof(num_strings));
 
     // TODO: Add loop to:
-    // 1. Read string length
-    // 2. Read string characters
-    // 3. Print string
+    for (unsigned int i = 0; i < num_strings; i++) {
+        // 1. Read string length
+        unsigned int str_len;
+        file.read(reinterpret_cast<char*>(&str_len), sizeof(str_len));
+        // 2. Read string characters
+        char* buffer = new char[str_len];
+        file.read(buffer, str_len);
+        // 3. Print string
+        std::cout << "String " << (i + 1) << ": ";;
+        for (int j = 0; j < str_len; j++) {
+            std::cout << buffer[j];
+        }
+        std::cout << std::endl;
+    }
 
     file.close();
     return 0;
